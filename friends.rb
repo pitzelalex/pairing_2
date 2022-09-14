c38a0605497ec0d2ab0931e55cabf17e84ff5b1c
Alex Pitzel <pitzelalex@gmail.com>
2022-09-14 17:29:47 -0400

    Add another friend variable

--- friends.rb
+++ friends.rb
@@ -1,6 +1,10 @@
-friend = "Jake"
+friend = "Mike"
+amigo = "Chris"
+
 if friend == "Ryan"
   puts "Hi best friend"
+elsif friend == amigo
+  puts "Hi very good friend"
 else
   puts "Hi friend"
 end
